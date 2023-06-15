c73cd437d222f5bc7a5e4ece76227b3b4f39f5f8
Nataly <120113884+Natalyname@users.noreply.github.com>
2023-06-15 10:45:38 +0300

    added main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+//js страницы
+
+const slider = new Slaider('.slider', {
+    width: 1080,
+    height: 720,
+});
+slider.init();
+
+// setInterval(() => {
+//     slider.setNextSlide();
+// }, 2000);
